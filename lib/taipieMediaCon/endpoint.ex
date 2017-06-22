2cc3d7c1fc1b43fc6842c82b5b9e7331c7b0f270
masato25 <leomasato25@gmail.com>
2017-06-23 00:12:51 +0800

    update gitignore && add file path to static routes

--- lib/taipieMediaCon/endpoint.ex
+++ lib/taipieMediaCon/endpoint.ex
@@ -9,7 +9,7 @@ defmodule TaipieMediaCon.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :taipieMediaCon, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico robots.txt file)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
